0ab286db4d0d68a87b9a3d48ff22b888727c3419
Michael Silva <michaels@ciandt.com>
2017-11-28 22:37:28 -0200

    Adding denpendencies to project

--- app/src/main/java/com/monstan/firetalk/LoginActivity.kt
+++ app/src/main/java/com/monstan/firetalk/LoginActivity.kt
@@ -1,10 +1,17 @@
 package com.monstan.firetalk
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.monstan.firetalk.R.layout.activity_login
 
 /**
  * @author michaels on 28/11/2017.
  */
 class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(activity_login)
+    }
 
 }
